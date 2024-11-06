ef77d6c81a15f90e32c3d371a33217f875e1b781
GABRIEL YAGO RODRIGUES LEAL <gabriel.yrleal@senacsp.edu.br>
2024-11-05 22:26:18 -0300

    atualizado

---
+++ app-maioridade/app.js
@@ -0,0 +1,24 @@
+/**
+ * Validação da maioridade
+ * @Author Gabriel Yago 
+ */
+
+// função  para validação de maioridade 
+function validar(event) {
+    // a linha abaixo desabilita o comportamento padrão de envio do formulario
+    event.preventDefault()
+    //alert("teste de envio")
+    let nome = document.getElementById('nome').value
+    //alert(nome)
+    //console.log(nome)
+    let idade = document.getElementById('idade').value
+
+    // Saida 
+    document.getElementById('aluno').value = `Aluno: ${nome}`
+
+    if (idade < 18) {
+        document.getElementById('status').value = "menor de idade"
+    } else {
+        document.getElementById('status').value = "Maior de idade"
+    }
+}
